49d1085c0acff5a7d281def0fe856728ef5aa514
mrueda020 <mrueda020@hotmail.com>
2021-04-14 18:06:45 -0500

    Added API Layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,12 +5,14 @@ import java.time.Month;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Make this class a Restful API
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
     @GetMapping
-    public List<Student> hello() {
+    public List<Student> getStudents() {
         return List.of(new Student(1L, "Miguel", "mrueda020@hotmail.com", 22, LocalDate.of(1998, Month.JULY, 3)));
 
     }

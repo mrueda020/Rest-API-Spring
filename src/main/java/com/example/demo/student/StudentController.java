e29d16edeaeb30a5df9892a8a99b1fa5a4624139
mrueda020 <mrueda020@hotmail.com>
2021-04-15 12:14:38 -0500

    Add updateStudent route

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,10 +3,14 @@ package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Make this class is a Restful API
@@ -29,5 +33,17 @@ public class StudentController {
     public void registerStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
 
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long StudentId) {
+        studentService.deleteStudent(StudentId);
+
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +28,32 @@ public class StudentService {
             throw new IllegalStateException("Email is taken");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long StudentId) {
+        boolean exists = studentRepository.existsById(StudentId);
+        if (!exists) {
+            throw new IllegalStateException("Student with id " + StudentId + " does not exists");
+        }
+        studentRepository.deleteById(StudentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exists"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentByemail = this.studentRepository.findStudentByEmail(email);
+            if (studentByemail.isPresent()) {
+                throw new IllegalStateException("Email is taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
